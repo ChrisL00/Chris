162e75a5f36093051149979bdb2efe19968b5280
ChrisL00 <chrislin0030@gmail.com>
2022-02-05 22:33:24 -0500

    abc

---
+++ scripts/01-data_cleaning.R
@@ -0,0 +1,36 @@
+#### Preamble ####
+# Purpose: Clean the survey data downloaded from Open Data Toronto
+# Author: Chenyu Lin
+# Data: 5 February 2022
+# Contact: chrischenyu.lin@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: 
+# - Need to have downloaded the ACS data and saved it to inputs/data
+# - Don't forget to gitignore it!
+# - Change these to yours
+# Any other information needed?
+
+
+#### Workspace setup ####
+# Use R Projects, not setwd().
+library(haven)
+library(tidyverse)
+# Read in the raw data. 
+raw_data <- readr::read_csv("inputs/data/raw_data.csv"
+                     )
+# Just keep some variables that may be of interest (change 
+# this depending on your interests)
+names(raw_data)
+
+reduced_data <- 
+  raw_data %>% 
+  select(first_col, 
+         second_col)
+rm(raw_data)
+         
+
+#### What's next? ####
+
+
+
+         
